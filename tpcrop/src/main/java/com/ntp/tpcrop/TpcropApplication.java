10bf1b8a7a051e6b73646b393470d815ff913073
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-08-15 01:11:59 +0700

    feat: initialize spring boot backend

---
+++ tpcrop/src/main/java/com/ntp/tpcrop/TpcropApplication.java
@@ -0,0 +1,13 @@
+package com.ntp.tpcrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpcropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpcropApplication.class, args);
+	}
+
+}
---
+++ tpcrop/src/test/java/com/ntp/tpcrop/TpcropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntp.tpcrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpcropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
