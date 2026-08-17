1c27b51049fb327a029dffe72b1b836400ad7c16
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-08-17 09:51:53 +0700

    feat: update mappers to ignore unmapped target policy

--- tpcrop/src/main/java/com/ntp/tpcrop/service/mapper/CropMapper.java
+++ tpcrop/src/main/java/com/ntp/tpcrop/service/mapper/CropMapper.java
@@ -1,11 +1,12 @@
 package com.ntp.tpcrop.service.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import com.ntp.tpcrop.dto.response.crop.CropViewDto;
 import com.ntp.tpcrop.entity.Crops;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CropMapper {
 
     CropViewDto toDto(Crops crop);
--- tpcrop/src/main/java/com/ntp/tpcrop/service/mapper/SeasonMapper.java
+++ tpcrop/src/main/java/com/ntp/tpcrop/service/mapper/SeasonMapper.java
@@ -2,11 +2,12 @@ package com.ntp.tpcrop.service.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
 import com.ntp.tpcrop.dto.response.season.SeasonViewDto;
 import com.ntp.tpcrop.entity.Seasons;
 
-@Mapper(componentModel = "spring", uses = {CropMapper.class})
+@Mapper(componentModel = "spring", uses = {CropMapper.class}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SeasonMapper {
 
     @Mapping(source = "cropId", target = "crop")
