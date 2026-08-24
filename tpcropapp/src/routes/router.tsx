d1db47b91ca186ad35c9a7ff55cb8d846dab8738
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-08-25 00:31:48 +0700

    feat(tpcropapp): implement registration page

--- tpcropapp/src/routes/router.tsx
+++ tpcropapp/src/routes/router.tsx
@@ -1,6 +1,7 @@
 import BasePage from "@/components/layout/BasePage";
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 path: "login",
                 element: <Login />,
+            },
+            {
+                path: "register",
+                element: <Register />,
             }
         ]
     }
