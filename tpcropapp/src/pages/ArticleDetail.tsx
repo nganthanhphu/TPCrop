5f2b253e454ea7f20376c7568e08a92eaf18701d
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-08-25 13:45:40 +0700

    refactor(tpcropapp): remove unused import

--- tpcropapp/src/pages/ArticleDetail.tsx
+++ tpcropapp/src/pages/ArticleDetail.tsx
@@ -3,7 +3,6 @@ import { useParams, Link, useNavigate } from "react-router-dom";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 import {
-    ArrowLeft,
     Heart,
     MessageSquare,
     Send,
