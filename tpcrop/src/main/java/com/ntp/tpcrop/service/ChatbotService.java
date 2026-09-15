c16fe3ebf4cd4db6f7ea4af184af05ba3aea1942
Ngàn Thành Phú <nhocjoon123@gmail.com>
2026-09-16 02:24:16 +0700

    feat: include user id in chat request body for chatbot service

--- tpcrop/src/main/java/com/ntp/tpcrop/service/ChatbotService.java
+++ tpcrop/src/main/java/com/ntp/tpcrop/service/ChatbotService.java
@@ -1,28 +1,41 @@
 package com.ntp.tpcrop.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
 import com.ntp.tpcrop.dto.request.ChatRequestDto;
 import com.ntp.tpcrop.dto.response.ChatResponseDto;
+import com.ntp.tpcrop.util.UserUtil;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 public class ChatbotService {
 
     private final RestClient restClient;
+    private final UserUtil userUtil;
 
-    public ChatbotService(@Value("${chatbot.api.url}") String baseUrl) {
+    public ChatbotService(@Value("${chatbot.api.url}") String baseUrl, UserUtil userUtil) {
         this.restClient = RestClient.builder()
                 .baseUrl(baseUrl)
                 .build();
+        this.userUtil = userUtil;
     }
 
     public ChatResponseDto ask(ChatRequestDto request) {
+        Long userId = userUtil.getCurrentUser().getId();
+        Map<String, ?> requestBody = Map.of(
+                "user_id", userId,
+                "question", request.question()
+        );
+
         try {
             ChatResponseDto response = restClient.post()
             .uri("/api/chat")
-            .body(request)
+            .body(requestBody)
             .retrieve()
             .body(ChatResponseDto.class);
             return response;
